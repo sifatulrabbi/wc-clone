ee47c42ee78248fa491bd376504203f1513e60d0
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-07-14 19:29:37 +0600

    process targeted file and output based on options

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -10,13 +11,15 @@ import (
 func main() {
 	var (
 		args     = os.Args[1:]
-		options  = []string{}
+		options  = map[string]string{}
 		filename = ""
 	)
 	for i := 0; i < len(args); i++ {
 		input := args[i]
 		if string(input[0]) == "-" {
-			options = append(options, strings.Split(input, "")[1:]...)
+			for _, op := range strings.Split(input, "")[1:] {
+				options[op] = op
+			}
 		} else {
 			filename = input
 		}
@@ -25,8 +28,8 @@ func main() {
 		log.Panicln("Please input the targeted file name!")
 	}
 
-	fmt.Printf("targeted file: '%s'\n", filename)
-	fmt.Printf("options: '%v'\n", options)
+	// fmt.Printf("targeted file: '%s'\n", filename)
+	// fmt.Printf("options: '%v'\n", options)
 
 	f, err := os.Stat(filename)
 	if os.IsNotExist(err) {
@@ -35,5 +38,51 @@ func main() {
 		log.Panicln(err)
 	}
 
-	fmt.Printf("targeted file size %d bytes\n", f.Size())
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	// bytes -c --bytes
+	// words -w --words
+	// chars -m --chars
+	// lines -l --lines
+	// max line length -L --max-line-length
+	// --help
+
+	var (
+		totalBytes    = f.Size()
+		totalChars    = 0
+		totalWords    = 0
+		totalLines    = 0
+		maxLineLength = 0
+		result        = "file: " + filename
+	)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		totalLines++
+		totalWords += strings.Count(line, " ")
+		totalChars += len(line)
+		if maxLineLength < len(line) {
+			maxLineLength = len(line)
+		}
+	}
+
+	result += fmt.Sprintf("\nlines: %d", totalLines)
+	for op := range options {
+		if op == "m" {
+			result += fmt.Sprintf("\nchars: %d", totalChars)
+		}
+		if op == "w" {
+			result += fmt.Sprintf("\nwords: %d", totalWords)
+		}
+		if op == "c" {
+			result += fmt.Sprintf("\nbytes: %d", totalBytes)
+		}
+		if op == "L" {
+			result += fmt.Sprintf("\nMax line length: %d", totalBytes)
+		}
+	}
+	fmt.Println(result)
 }
